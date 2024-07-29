0bccec31dfe3be63f69ef6df155a884ca92409a5
Henri Galdámez <estuardo.galdamezedumail.com>
2024-07-29 15:12:34 -0600

    First Commit

---
+++ Estudiante.java
@@ -0,0 +1,45 @@
+package TareasEntregables_PrincipiosDePoo;
+import java.util.Date;
+
+
+public class Estudiante extends Persona {
+    private Estado estado;
+    private Date fechaDeNacimiento;
+
+
+    public enum Estado {
+        MATRICULADO, INACTIVO, GRADUADO;
+    }
+
+    public Estudiante(int id, String nombre, String apellido, Date fechaDeNacimiento, Estado estado) {
+        super(id, nombre, apellido);
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.estado = estado;
+    }
+
+    public Date getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public void cambiarEstado(Estado nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+
+    @Override
+    public String toString() {
+        return "                               estudiantes:       "+"\n"+
+                "id: "+getId() + "* nombre: "+getNombre() + "* apellido: "+getApellido() + "* Fecha de nacimiento: "+fechaDeNacimiento+ "* estado: "+estado;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,42 @@
+package TareasEntregables_PrincipiosDePoo;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante(1, "Pablo", "Hérnandez", new Date(), Estudiante.Estado.MATRICULADO);
+        Estudiante estudiante2 = new Estudiante(2, "David", "Monrroy", new Date(), Estudiante.Estado.INACTIVO);
+
+        Curso curso1 = new Curso(1, "Matematicas", "Programación Matemática", 3, "1,0");
+        Curso curso2 = new Curso(2, "Ingenieria", "Cursos 3D", 2, "1,1");
+        GestorAcademico gestor = new GestorAcademico();
+
+        gestor.matricularEstudiante(estudiante1);
+        gestor.matricularEstudiante(estudiante2);
+        gestor.agregarCurso(curso1);
+        gestor.agregarCurso(curso2);
+        try {
+            gestor.inscribirEstudianteCurso(estudiante1, curso1.getId());
+            gestor.inscribirEstudianteCurso(estudiante2, curso2.getId());
+            gestor.inscribirEstudianteCurso(estudiante1, curso1.getId());
+        } catch (EstudianteYaInscritoException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            gestor.desinscribirEstudianteCurso(estudiante1.getId(), curso1.getId());
+            gestor.desinscribirEstudianteCurso(estudiante1.getId(), curso1.getId());
+        } catch (EstudianteNoInscritoEnCursoException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Estudiantes matriculados:"+"\n");
+        for (Estudiante est : gestor.getEstudiantes()) {
+            System.out.println(est);
+        }
+        System.out.println("Cursos disponibles:"+"\n");
+        for (Curso cur : gestor.getCursos()) {
+            System.out.println(cur);
+        }
+    }
+}
+
+
